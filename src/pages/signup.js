78aa842f09bf3c442fcf215005f0516ce0f3eb0c
RIGUANG JIN <bosco@RIGUANGs-iMac.local>
2020-08-07 21:13:24 -0400

    fix sign up and AuthRoute

--- src/pages/signup.js
+++ src/pages/signup.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import AppIcon from '../images/icon.png';
-import axios from 'axios';
 import { Link } from 'react-router-dom';
 // MUI
 import { withStyles } from '@material-ui/core/styles';
@@ -10,6 +9,10 @@ import Typography from '@material-ui/core/Typography';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import CircularProgress from '@material-ui/core/CircularProgress';
+
+// redux
+import { connect } from 'react-redux';
+import { signupUser } from '../redux/actions/userActions';
 
 
 const styles = (theme) => ({
@@ -25,10 +28,16 @@ class signup extends Component {
             password: '',
             confirmPassword: '',
             handle: '',
-            loading: false,
             errors: {}
         }
     }
+
+    componentWillReceiveProps(nextProps) {
+        if(nextProps.UI.errors) {
+            this.setState({errors: nextProps.UI.errors})
+        }
+    }
+
     handleSubmit = (event) => {
         event.preventDefault();
         this.setState({
@@ -40,21 +49,7 @@ class signup extends Component {
             confirmPassword: this.state.confirmPassword,
             handle: this.state.handle
         }
-        axios.post('/signup', newUserData)
-            .then(res => {
-                console.log(res.data);
-                localStorage.setItem('FBIdToken', `Bearer ${res.data.token}`);
-                this.setState({
-                    loading: false
-                });
-                this.props.history.push('/')
-            })
-            .catch(err => {
-                this.setState({
-                    errors: err.response.data,
-                    loading: false
-                })
-            })
+        this.props.signupUser(newUserData, this.props.history)
     };
     
     handleChange = (event) => {
@@ -66,8 +61,8 @@ class signup extends Component {
 
 
     render() {
-        const { classes } = this.props;
-        const { errors, loading } = this.state;
+        const { classes, UI: { loading } } = this.props;
+        const { errors } = this.state;
 
         return (
             <Grid container className={classes.form}>
@@ -150,7 +145,16 @@ class signup extends Component {
 }
 
 signup.propTypes = {
-    classes: PropTypes.object.isRequired
+    classes: PropTypes.object.isRequired,
+    user: PropTypes.object.isRequired,
+    UI: PropTypes.object.isRequired,
+    logoutUser: PropTypes.func.isRequired
 }
 
-export default withStyles(styles)(signup);
+const mapStateToProps = (state) => ({
+    user: state.user,
+    UI: state.UI
+})
+
+
+export default connect( mapStateToProps, { signupUser } )(withStyles(styles)(signup));
--- src/redux/actions/userActions.js
+++ src/redux/actions/userActions.js
@@ -1,4 +1,4 @@
-import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI } from '../types';
+import { SET_USER, SET_ERRORS, CLEAR_ERRORS, LOADING_UI, SET_UNAUTHENTICATED } from '../types';
 
 import axios from 'axios';
 
@@ -8,9 +8,7 @@ export const loginUser = (userData, history) => (dispatch) => {
     axios.post('/login', userData)
         .then(res => {
             
-            const FBIdToken = `Bearer ${res.data.token}`;
-            localStorage.setItem('FBIdToken', FBIdToken);
-            axios.defaults.headers.common['Authorization'] = FBIdToken;
+            setAuthorizationHeader(res.data.token)
             dispatch(getUserData());
             dispatch({type: CLEAR_ERRORS});
             history.push('/')
@@ -34,3 +32,36 @@ export const getUserData = () => (dispatch) => {
         })
         .catch(err => console.log(err));
 }
+
+export const logoutUser = () => (dispatch) => {
+    localStorage.removeItem('FBIdToken');
+    delete axios.defaults.headers.common['Authorization'];
+    dispatch({ type: SET_UNAUTHENTICATED })
+}
+
+
+export const signupUser = (newUserData, history) => (dispatch) => {
+    dispatch({ type: LOADING_UI });
+
+    axios.post('/signup', newUserData)
+        .then(res => {
+            
+            setAuthorizationHeader(res.data.token);
+            dispatch(getUserData());
+            dispatch({type: CLEAR_ERRORS});
+            history.push('/')
+        })
+        .catch(err => {
+            dispatch({
+                type: SET_ERRORS,
+                payload: err.response.data
+            })
+        })
+        
+}
+
+const setAuthorizationHeader = (token) => {
+    const FBIdToken = `Bearer ${token}`;
+    localStorage.setItem('FBIdToken', FBIdToken);
+    axios.defaults.headers.common['Authorization'] = FBIdToken;
+};
